d9e6e130de8320670d592f56191f4aa8a4da06ae
Dennis Smith <dennis.smith@omron.com>
2020-03-13 10:18:20 -0700

    Added drivers

--- drivers/hv/channel_mgmt.c
+++ drivers/hv/channel_mgmt.c
@@ -1022,24 +1022,9 @@ void vmbus_hvsock_device_unregister(struct vmbus_channel *channel)
 {
 	BUG_ON(!is_hvsock_channel(channel));
 
-	for (;;) {
-		if (!READ_ONCE(channel->probe_done)) {
-			msleep(1);
-			continue;
-		}
-
-		/* We always get a rescind msg when a connection is closed. */
-		if (READ_ONCE(channel->rescind))
-			break;
-
-		if (hv_get_bytes_to_read(&channel->outbound) == 0)
-			break;
-
+	/* We always get a rescind msg when a connection is closed. */
+	while (!READ_ONCE(channel->probe_done) || !READ_ONCE(channel->rescind))
 		msleep(1);
-	}
-
-	/* FIXME: in case vmbus_onoffer_rescin() runs too slow */
-	channel->rescind = true;
 
 	vmbus_device_unregister(channel->device_obj);
 }
